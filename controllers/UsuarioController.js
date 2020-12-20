6f057442eea1bb8c871b7b8818ef025c535fbcb1
Gustavo García <gustavogarcia@utp.edu.co>
2020-12-19 19:46:19 -0500

    Se actualizaron las rutas del usuario

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -11,7 +11,7 @@ module.exports = {
       if (user) {
         let match = bcrypt.compareSync(req.body.password, user.password);
         if (match) {
-          let tokenReturn = await token.encode(user.id,user.rol);
+          let tokenReturn = await token.encode(user.id, user.rol);
           return res.status(200).json({ user, tokenReturn });
         } else {
           return res.status(401).send({
@@ -30,6 +30,86 @@ module.exports = {
         message: "Ocurrió un error",
       });
       next(e);
+    }
+  },
+
+  // Listar todos los usuarios
+  list: async (req, res, next) => {
+    try {
+      const reg = await models.Usuario.findAll();
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({
+        message: "Se ha producido un error",
+      });
+      next(error);
+    }
+  },
+
+  //nos permite agregar un nuevo usuario
+  add: async (req, res, next) => {
+    try {
+      req.body.password = bcrypt.hashSync(req.body.password, 10); //10 numero de veces que encriptara la contraseña
+      const reg = await models.Usuario.create(req.body);
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({
+        message: "Ocurrió un error",
+      });
+      next(error);
+    }
+  },
+
+  //actualizar un usuario especifico según su id
+  update: async (req, res, next) => {
+    try {
+      const reg = await models.Usuario.update(
+        {
+          nombre: req.body.nombre,
+          email: req.body.email,
+          rol: req.body.rol,
+          password: bcrypt.hashSync(req.body.password, 10),
+        },
+        { where: { id: req.body.id } }
+      );
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({
+        message: "Ocurrió un error",
+      });
+      next(error);
+    }
+  },
+
+  //cambiar al estado activo(1) el usuario
+  activate: async (req, res, next) => {
+    try {
+      const reg = await models.Usuario.update(
+        { estado: 1 },
+        { where: { id: req.body.id } }
+      );
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({
+        message: "Ocurrió un error",
+      });
+      next(error);
+    }
+  },
+
+  //cambiar al estado desactivado(0) el usuario
+  deactivate: async (req, res, next) => {
+    try {
+      const reg = await models.Articulo.update(
+        { estado: 0 },
+        { where: { id: req.body.id } }
+      );
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({
+        message: "Ocurrió un error",
+      });
+      next(error);
     }
   },
 };
